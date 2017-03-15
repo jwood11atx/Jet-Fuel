556341286ee6221fed6e42e79c258fdd70c0d630
Justin Wood <jwood11atx@gmail.com>
2017-03-15 00:27:24 -0600

    add class toggle to highlight which folder is currently selected.

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.static(__dirname + "/app"));
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.locals.folder = [];
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "app", "index.html"))
+  res.sendFile(path.join(__dirname, "app", "index.html"));
 });
 
 app.post("/folders", (req, res) => {
-  app.locals.folder.push({[req.body.folderName]: {}})
-  res.json({folders: app.locals.folder})
-})
+  app.locals.folder.push({[req.body.folderName]: {}});
+  res.json({folders: app.locals.folder});
+});
 
 app.get("/folders", (req, res) => {
-  res.json({folders: app.locals.folder})
-})
+  res.json({folders: app.locals.folder});
+});
 
 app.set("port", process.env.PORT || 3000);
 
 app.listen(app.get("port"), () => {
   console.log(`server up, listening at port:3000`);
-})
+});

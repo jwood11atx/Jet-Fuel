8e61fba9dde57661ed22b777c60f46d756a9805e
Justin Wood <jwood11atx@gmail.com>
2017-03-14 23:46:53 -0600

    add GET call to fetch folders when page is loaded

--- app/main.js
+++ app/main.js
@@ -1,3 +1,16 @@
+window.onload = () => {
+  fetch("http://localhost:3000/folders")
+  .then(res => res.json())
+  .then(json => {
+    let displayFolderNames = json.folders.map(folder => {
+      for(key in folder){
+        return `<article>${key}</article>`;
+      }
+    });
+    document.getElementById("right-section").innerHTML = displayFolderNames.join("");
+  });
+};
+
 document.getElementById("folder-submit").addEventListener("click", () => {
   const folderName = document.getElementById("folder-input").value;
   fetch("http://localhost:3000/folders", {

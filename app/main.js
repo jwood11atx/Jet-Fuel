4f66e2de4db0a36a69aa8b740368713648a4e109
Justin Wood <jwood11atx@gmail.com>
2017-03-14 23:35:27 -0600

    add endpoint /folders for user to POST new folder names. create main.js to connect inputfield and button to make the post call.

--- app/main.js
+++ app/main.js
@@ -1,1 +1,20 @@
-
+document.getElementById("folder-submit").addEventListener("click", () => {
+  const folderName = document.getElementById("folder-input").value;
+  fetch("http://localhost:3000/folders", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({folderName})
+  })
+    .then(res => res.json())
+    .then(json => {
+      let displayFolderNames = json.folders.map(folder => {
+        for(key in folder){
+          return `<article>${key}</article>`;
+        }
+      });
+      document.getElementById("right-section").innerHTML = displayFolderNames.join("");
+    });
+});

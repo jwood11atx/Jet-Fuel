d16b48c839cdfb357b9f58bf40ee43839671c36f
Justin Wood <jwood11atx@gmail.com>
2017-03-16 16:22:41 -0600

    add const to line 7

--- test/folders-test.js
+++ test/folders-test.js
@@ -4,7 +4,7 @@ const {displayErrMsg} = require("../app/helper/folders.js");
 
 describe("folders helper functions", () => {
   it("exists?", () => {
-    errMsg = {innerHTML: ""};
+    const errMsg = {innerHTML: ""};
     const message = "test error msg";
     displayErrMsg(message, errMsg);
     expect(errMsg.innerHTML).to.equal(message);

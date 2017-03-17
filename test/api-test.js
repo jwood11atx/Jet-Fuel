5a43ccecebe509de6dbf0ad75dcdb2c26be09feb
Justin Wood <jwood11atx@gmail.com>
2017-03-17 04:56:15 -0600

    add tests for helper url functions

---
+++ test/api-test.js
@@ -0,0 +1,7 @@
+const {expect} = require("chai");
+
+// const {selectFolder, createFolder, folderCheck, displayFolders, reselectFolder} = require("../app/helpers/api.js");
+
+describe("helpers/api.js", () => {
+
+});
